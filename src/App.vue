d98925258e61edd2a077a995176f23748b56c788
advancingKoala <milaiduoduo4567@163.com>
2018-05-03 11:26:07 +0800

    在全局，设置了激活link的样式。在此branch增加了nav组件，并运用到app.vue上，且如果组件找不到，会被导航到404页面。

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
+    <m-nav></m-nav>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+  import mNav from './components/nav';
+  export default{
+    components: {mNav}
+  }
 </script>
 
 <style>
---
+++ src/components/nav.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <router-link tag="li" to="/home">home</router-link>
+    <router-link tag="li" to="/project">project</router-link>
+    <router-link tag="li" to="/about">about</router-link>
+  </ul>
+</template>
+<script type='text/ecmascript-6'>
+
+</script>
+<style>
+  ul{background: #ccc;}
+  ul li {
+    display: inline-block;
+    width: 100px;
+    height: 30px;
+    line-height: 30px;
+    text-align: center;
+    list-style: none;
+    cursor: pointer;
+  }
+  /*ul li:hover{*/
+    /*background: yellow;*/
+  /*}*/
+</style>
