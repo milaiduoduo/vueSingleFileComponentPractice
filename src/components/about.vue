15fe580de519d6e5dfc220fd60108b9a5df9356b
advancingKoala <milaiduoduo4567@163.com>
2018-05-03 17:50:25 +0800

    新建about的二级导航，引入scss来书写样式

---
+++ src/components/about.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>i am about</div>
+</template>
+<script type='text/ecmascript-6'>
+
+</script>
+<style lang="scss" type="text/css">
+
+</style>
--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,23 +1,69 @@
 <template>
-  <ul>
-    <router-link exact tag="li" to="/home">home</router-link>
-    <router-link exact active-class="navLink" tag="li" to="/project">project</router-link>
-    <router-link exact tag="li" to="/about">about</router-link>
+  <ul class="nav">
+    <router-link exact tag="li" to="/home">
+      <a>home</a>
+    </router-link>
+    <router-link exact active-class="navLink" tag="li" to="/project">
+      <a>project</a>
+    </router-link>
+    <router-link exact tag="li" to="/about">
+      <a>about</a>
+      <ul class="nav-l2">
+        <router-link to="/about/profile" tag="li">profile</router-link>
+        <router-link to="/about/experience" tag="li">experience</router-link>
+        <router-link to="/about/taget" tag="li">taget</router-link>
+      </ul>
+    </router-link>
   </ul>
 </template>
 <script type='text/ecmascript-6'>
-
+  //  import about from './about';
+  //  export default{
+  //    components: {about}
+  //  }
 </script>
-<style>
-  ul{background: #ccc;}
-  ul li {
-    display: inline-block;
-    width: 100px;
-    height: 30px;
-    line-height: 30px;
-    text-align: center;
-    list-style: none;
-    cursor: pointer;
+<style lang="scss" type="text/css">
+  .nav {
+    background: #ccc;
+  >li {
+     position:relative;
+     display: inline-block;
+     width: 100px;
+     height: 30px;
+     line-height: 30px;
+     text-align: center;
+     list-style: none;
+     cursor: pointer;
+  > a {
+      display: block;
+      width: 100%;
+      height: 100%;
+      color:#fff;
+      text-decoration: none;
+    }
   }
-  .navLink{color:#fff;background: blueviolet;}
+  }
+
+  a:link,a:hover,a:active,a:focus{
+    color:#fff;
+  }
+
+  .navLink {
+    color: #fff;
+    background: blueviolet;
+  }
+
+  .nav-l2 {
+    position: absolute;
+    width:100%;
+    border:1px solid #ccc;
+    border-top:none;
+  >li{
+     color:#7a7a7a;
+  &:hover,&.is-active{
+     color:#fff;
+     background: deepskyblue;
+   }
+  }
+  }
 </style>
