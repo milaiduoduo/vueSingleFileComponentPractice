0d0b92252520443fb6a045ef21bb67a9e162bf29
advancingKoala <milaiduoduo4567@163.com>
2018-05-08 11:46:01 +0800

    给各导航链接页面添加淡入淡出的过渡动画效果

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div>
     <m-nav></m-nav>
-    <router-view class="center"></router-view>
-    <router-view name="slider"></router-view>
+    <transition mode="out-in">
+      <router-view class="center"></router-view>
+      <router-view name="slider"></router-view>
+    </transition>
   </div>
 </template>
 
@@ -13,6 +15,14 @@
   }
 </script>
 
-<style>
-  .center{text-align: center;}
+<style lang="scss" rel="stylesheet/scss">
+  .center {
+    text-align: center;
+  }
+  .v-enter{opacity: 0;}
+  .v-enter-to{opacity: 1;}
+  .v-enter-active{transition:1s;}
+  .v-leave{opacity: 1;}
+  .v-leave-to{opacity: 0;}
+  .v-leave-active{transition:1s;}
 </style>
